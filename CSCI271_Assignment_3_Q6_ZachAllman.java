d824fe463bf4b42fd2615af6bb892a00d7672d57
allmanz2 <allmanz2@winthrop.edu>
2026-02-25 16:18:14 -0500

    Last file, did the same thing as 1,2,3,4,5.

---
+++ CSCI271_Assignment_3_Q6_ZachAllman.java
@@ -0,0 +1,37 @@
+/*************************************************************************
+* Assignment 3, Question 6 for CSCI 271-001 Spring 2026
+*
+* Author: Zach Allman
+* OS: Ubuntu 24.04.3 LTS
+* Compiler: javac 21.0.9
+* Date: February 25 2026
+*
+* Purpose
+* This program reads a list of integers from the keyboard and stores
+* them into an array A, then returns the sum of all even integers in A
+* using a recursive function sumEven(). The function is recursive and
+* does not print anything to the screen; only the main program prints
+* the resulting sum. No loops are used in the sumEven function.
+*
+*************************************************************************/
+/*************************************************************************
+*
+* I declare and confirm the following:
+* - I have not discussed this program code with anyone other than my
+*   instructor or the teaching assistants assigned to this course.
+* - I have not used programming code obtained from someone else,
+*   or any unauthorised sources, including the Internet, either
+*   modified or unmodified.
+* - If any source code or documentation used in my program was
+*   obtained from other sources, like a text book or course notes,
+*   I have clearly indicated that with a proper citation in the
+*   comments of my program.
+* - I have not designed this program in such a way as to defeat or
+*   interfere with the normal operation of the supplied grading code.
+*
+* Zach Allman
+********************************************************************/
+import java.util.Scanner;
+
+public class CSCI271_Assignment3_Q6_ZachAllman
+{
