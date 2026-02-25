ed29c5443d8d5acfe1ada97b99b33f945ea0b0b6
allmanz2 <allmanz2@winthrop.edu>
2026-02-25 16:01:56 -0500

    Same thing I did for file one

---
+++ CSCI271_Assignment_3_Q2_ZachAllman.java
@@ -0,0 +1,36 @@
+/*************************************************************************
+* Assignment 3, Question 2 for CSCI 271-001 Spring 2026
+*
+* Author: Zach Allman
+* OS: Ubuntu 24.04.3 LTS
+* Compiler: javac 21.0.9
+* Date: February 25 2026
+*
+* Purpose
+* This program reads a string S and a single character C from the user
+* and displays the number of times C occurs in S using a recursive
+* function. The function takes S and C as arguments and returns the
+* count recursively without using any loops.
+*
+*************************************************************************/
+/*************************************************************************
+*
+* I declare and confirm the following:
+* - I have not discussed this program code with anyone other than my
+*   instructor or the teaching assistants assigned to this course.
+* - I have not used programming code obtained from someone else,
+*   or any unauthorised sources, including the Internet, either
+*   modified or unmodified.
+* - If any source code or documentation used in my program was
+*   obtained from other sources, like a text book or course notes,
+*   I have clearly indicated that with a proper citation in the
+*   comments of my program.
+* - I have not designed this program in such a way as to defeat or
+*   interfere with the normal operation of the supplied grading code.
+*
+* Zach Allman
+********************************************************************/
+import java.util.Scanner;
+
+public class CSCI271_Assignment3_Q2_ZachAllman
+{
